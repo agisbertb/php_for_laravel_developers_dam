e70a70bfa921fd80cb8cbde05d0b0ae2f767bf65
agisbertb <agisbert@iesebre.com>
2023-09-20 17:49:43 +0200

    Afegida la funció connectDB

--- app/helpers.php
+++ app/helpers.php
@@ -12,3 +12,15 @@ function dd($xivato)
     var_dump($xivato);
     die();
 }
+
+function connectDB($config)
+{
+    try {
+        return new PDO(
+            $config['database']['databasetype'] . ':host=' . $config['database']['host'] . ';dbname=' . $config['database']['name'],
+            $config['database']['user'],
+            $config['database']['password']);
+    } catch (\Exception $e) {
+        echo 'Error de connexió a la base de dades';
+    }
+}
--- app/index.php
+++ app/index.php
@@ -1,23 +1,12 @@
 <?php
 
+require 'config.php';
+
 require 'app/helpers.php';
 
 require 'app/Task.php';
 
-require 'config.php';
-
-//$user = 'debian-sys-maint';
-//$pass = '5ixSMGO5pVdzS2lt';
-//$dsn = 'mysql:host=localhost;dbname=phplaraveldevs';
-
-try {
-    $dbh = new PDO(
-        $config['database']['databasetype'] . ':host=' . $config['database']['host'] . ';dbname=' . $config['database']['name'],
-        $config['database']['user'],
-        $config['database']['password']);
-} catch (\Exception $e) {
-    echo 'Error de connexió a la base de dades';
-}
+$dbh = connectDB($config);
 
 $statement = $dbh->prepare('SELECT * FROM tasks;');
 
